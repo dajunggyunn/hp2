3531ce8d564e45640a7bd6f0148c77cdb3e0a2bd
dajunggyunn <gyen530@bigvalue.co.kr>
2022-12-06 19:39:40 +0900

    first commit for react

---
+++ src/main/java/com/bv/hp2/Hp2Application.java
@@ -0,0 +1,13 @@
+package com.bv.hp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hp2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/bv/hp2/Hp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bv.hp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
